50383c8a74353cb589e41f8d5bb7a3658689f4aa
Kosades <pronpro@mail.ru>
2021-02-12 00:41:10 +0300

    Добавление урока 8

---
+++ Lesson_8/Lesson_8/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lesson_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (string.IsNullOrEmpty(Properties.Settings.Default.UserName))
+            {
+
+                Console.WriteLine("Введите имя пользователя:");
+                Properties.Settings.Default.UserName = Console.ReadLine();
+                Properties.Settings.Default.Save();
+            }
+            string userName = Properties.Settings.Default.UserName;
+            string greeting = Properties.Settings.Default.Greeting;
+            Console.WriteLine($"{greeting}, {userName}!");
+
+        }
+    }
+}
